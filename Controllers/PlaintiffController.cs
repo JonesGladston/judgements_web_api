9998d25084efb4cb59b21422e9fdf44c0d346bbb
JonesGladston <jones.gladiator@gmail.com>
2020-03-02 14:09:46 +0530

    added plaintiffs controller

---
+++ Controllers/PlaintiffController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using webApiApp.Models;
+namespace webApiApp.Controllers
+{
+    [Route("Plaintiffs")]
+    [ApiController]
+    public class PlaintiffController : ControllerBase
+    {
+        private readonly judgement_dbContext _context;
+        public PlaintiffController(judgement_dbContext context)
+        {
+            _context = context;
+            if (_context.Plaintiffs.Count() == 0)
+            {
+                _context.Plaintiffs.Add(new Plaintiffs { FirstName = "tester" }); _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<Plaintiffs>> GetAll()
+        {
+            return _context.Plaintiffs.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Plaintiffs> GetById(long id)
+        {
+            var item = _context.Plaintiffs.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+
+}
