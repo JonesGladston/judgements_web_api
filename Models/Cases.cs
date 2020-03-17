0500fc3b3cbe221c52d5ce432b81cf8d0328489d
JonesGladston <jones.gladiator@gmail.com>
2020-03-17 13:54:18 +0530

    Search implementation for api

--- Models/Cases.cs
+++ Models/Cases.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace webApiApp.Models
 {
@@ -18,5 +19,50 @@ namespace webApiApp.Models
         public string CaseUrl { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+    }
+
+    public class CaseData
+    {
+        public long TotalCount { get; set; }
+
+        public List<Cases> Cases { get; set; }
+    }
+
+    public class SortCases
+    {
+
+        public IQueryable<Cases> Sort(string sort, IQueryable<Cases> data)
+        {
+            IQueryable<Cases> totalCases = data;
+            switch (sort)
+            {
+                case "CaseNo_asc":
+                    totalCases = totalCases.OrderBy(Case => Case.CaseNo);
+                    break;
+                case "CaseNo_dsc":
+                    totalCases = totalCases.OrderByDescending(Case => Case.CaseNo);
+                    break;
+                case "CaseType_asc":
+                    totalCases = totalCases.OrderBy(Case => Case.CaseType);
+                    break;
+                case "CaseType_dsc":
+                    totalCases = totalCases.OrderByDescending(Case => Case.CaseType);
+                    break;
+                case "FillingDate_asc":
+                    totalCases = totalCases.OrderBy(Case => Case.FillingDate);
+                    break;
+                case "FillingDate_dsc":
+                    totalCases = totalCases.OrderByDescending(Case => Case.FillingDate);
+                    break;
+                case "Judge_asc":
+                    totalCases = totalCases.OrderBy(Case => Case.Judge);
+                    break;
+                case "Judge_dsc":
+                    totalCases = totalCases.OrderByDescending(Case => Case.Judge);
+                    break;
+            }
+
+            return totalCases;
+        }
     }
 }
